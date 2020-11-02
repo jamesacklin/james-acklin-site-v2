0c67d690245f94e408d9d83481001fa8e8d2f72e
James Acklin <james.acklin@gmail.com>
2020-11-02 16:40:08 -0500

    initial pass with airtable data and styles

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-props-no-spreading': ['off'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,93 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import 'tachyons';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+import ClientLink from '../components/ClientLink';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query ProjectQuery {
+      allAirtable(
+        filter: { table: { eq: "Projects" } }
+        sort: { fields: data___Start_Date, order: DESC }
+      ) {
+        edges {
+          node {
+            data {
+              Client
+              Project_Name
+              Role_Work
+              End_Date(formatString: "")
+              Start_Date
+            }
+            id
+          }
+        }
+      }
+    }
+  `);
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  return (
+    <div>
+      <Helmet
+        style={[
+          {
+            cssText: `
+          .sans-serif {
+            font-family: "Inter";
+          }
+        `,
+          },
+        ]}
+      />
+      <table className="collapse">
+        <thead>
+          <tr>
+            <td className="pa2 sans-serif b black">Project</td>
+            <td className="pa2 sans-serif b black">Client</td>
+            <td className="pa2 sans-serif b black">Role</td>
+            <td className="pa2 sans-serif b black">Start ▼</td>
+            <td className="pa2 sans-serif b black">End</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allAirtable.edges.map((edge) => (
+            <tr className="" key={edge.node.id}>
+              <td className="bt bw1 sans-serif f5 pa2 black">
+                {edge.node.data.Project_Name}
+              </td>
+              <td className="bt bw1 sans-serif f5 pa2 black">
+                {edge.node.data.Client.map((client, i) => [
+                  i > 0 && ', ',
+                  <ClientLink key={client} clientId={client} />,
+                ])}
+              </td>
+              <td className="bt bw1 sans-serif f5 pa2 black">
+                {edge.node.data.Role_Work.sort().map((role) => (
+                  <span
+                    className={`sans-serif f6 br-pill ba bw1 ph3 pv2 mb1 mr2 dib ${
+                      role === 'UX Designer'
+                        ? 'white bg-black'
+                        : 'black bg-white'
+                    }`}
+                    key={role}
+                  >
+                    {role}
+                  </span>
+                ))}
+              </td>
+              <td className="bt bw1 sans-serif f5 nowrap pa2 black">
+                {edge.node.data.Start_Date}
+              </td>
+              <td className="bt bw1 sans-serif f5 nowrap pa2 black">
+                {edge.node.data.End_Date}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  );
+};
 
-export default IndexPage
+export default IndexPage;
