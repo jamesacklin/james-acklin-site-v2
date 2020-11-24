5a97eab78a94da136dce3d7ddb53a58c9272063e
James Acklin <james.acklin@gmail.com>
2020-11-24 16:57:14 -0500

    beginning of airtable query from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allAirtable(filter: { table: { eq: "Projects" } }) {
+          nodes {
+            data {
+              Imagery {
+                url
+              }
+            }
+          }
+        }
+      }
+    `).then(({ errors, data }) => {
+      if (errors) {
+        reject(errors);
+      }
+
+      data.allAirtable.nodes.map((node) => {
+        if (node.data.Imagery !== null) {
+          node.data.Imagery.map((image) => {
+            console.log(image.url);
+          });
+        }
+      });
+
+      resolve();
+    });
+  });
+};
