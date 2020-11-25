3bbdc52dc2ee1334e069e13257b2bbd3ea4068b4
James Acklin <james.acklin@gmail.com>
2020-11-25 12:31:49 -0500

    write attachments to disk

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const del = require('del');
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -9,6 +14,24 @@
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
 
+  const imageryPublicPath = path.resolve(process.cwd(), 'public/imagery');
+  var exists = fs.existsSync(imageryPublicPath);
+  if (exists) {
+    async () => {
+      try {
+        await del(imageryPublicPath);
+        console.log('deleted public imagery directory');
+        fs.mkdirSync(imageryPublicPath);
+      } catch (err) {
+        console.error(
+          'there was an error deleting the public imagery directory',
+        );
+      }
+    };
+  } else {
+    fs.mkdirSync(imageryPublicPath);
+  }
+
   return new Promise((resolve, reject) => {
     graphql(`
       {
@@ -17,6 +40,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             data {
               Imagery {
                 url
+                filename
               }
             }
           }
@@ -29,8 +53,25 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
       data.allAirtable.nodes.map((node) => {
         if (node.data.Imagery !== null) {
-          node.data.Imagery.map((image) => {
-            console.log(image.url);
+          node.data.Imagery.map((image, i) => {
+            const imgPath = path.resolve(
+              process.cwd(),
+              'public/imagery',
+              `${image.filename}`,
+            );
+
+            const request = https.get(image.url, (res) => {
+              let imageData = '';
+              res.setEncoding('binary');
+              res.on('data', (chunk) => {
+                imageData += chunk;
+              });
+              res.on('end', () => {
+                fs.writeFileSync(imgPath, imageData, 'binary');
+              });
+            });
+
+            // fs.writeFileSync(imgPath, Buffer, 'binary');
           });
         }
       });
